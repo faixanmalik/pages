163de6ad2ab3a00483dc00bbdea84c923bcc3cb7
faixanmalik <faixan.malik554@gmail.com>
2023-09-03 16:13:42 +0500

    button change

--- src/pages/menu/products.js
+++ src/pages/menu/products.js
@@ -131,7 +131,7 @@ const Products = () => {
 
         <div className='flex justify-between items-center px-10 bg-gray-200 py-2'>
             <button type="submit" className='flex p-[8px] rounded-md px-5 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
-                <AiOutlineCloseCircle className='text-xl mt-1'/>Close
+                Close <AiOutlineCloseCircle className='text-xl mt-1'/>
             </button>
             <div className='mr-20'>
                 <h1 className='font-medium text-gray-900 whitespace-nowrap text-sm'>number of items: {tableData.length}</h1>
