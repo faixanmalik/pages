0dfb2f1ba0d4a75808f9d91d15079697165f9c13
faixanmalik <faixan.malik554@gmail.com>
2023-09-09 19:31:08 +0500

    some change in desinging

--- src/pages/menu/invoices.js
+++ src/pages/menu/invoices.js
@@ -45,7 +45,7 @@ const Invoices = () => {
             <div className='w-full space-y-3 items-center'>
 
               <div className='flex justify-between'>
-                <div className='flex space-x-3 items-center'>
+                <div className='flex space-x-3 items-center w-[200px]'>
                   <label className="font-semibold text-sm text-gray-600 pb-1">Set Printing</label>
                   <label className="font-semibold text-sm text-gray-600 pb-1">1</label>
                   <input checked id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
@@ -53,53 +53,23 @@ const Invoices = () => {
                   <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                 </div>
 
-                <div className='flex items-center w-1/4 gap-2'>
-                  <label className="font-semibold w-32 text-sm text-gray-600 pb-1">Auto Numerator</label>
-                  <input placeholder='12345' name="autoNumerator" id='autoNumerator' type="number" className="px-3 py-2 w-[160px] mt-1 mb-4 text-sm ml-auto bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                <div className='flex flex-col w-[130px]'>
+                  <label className="font-semibold text-sm text-gray-600 pb-1">Auto Numerator</label>
+                  <input placeholder='12345' name="autoNumerator" id='autoNumerator' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
               </div>
 
-              <div className='flex space-x-3 justify-between items-center'>
 
-                <div className='flex items-center gap-2'>
-                  <label className="font-semibold text-sm text-gray-600 pb-1">Product Number</label>
+              <div className='flex space-x-5 justify-between'>
+
+                <div className='flex flex-col w-1/3'>
+                  <label className="font-semibold text-sm text-left text-gray-600 pb-1">Product Number</label>
                   <input placeholder='12345' name="productNumber" id='productNumber' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex items-center'>
-                  <label className="font-semibold w-52 text-sm text-gray-600 pb-1">Customer Phone No</label>
-                  <select
-                    id="customerPhone"
-                    name="customerPhone"
-                    className="px-3 py-2 mt-1 mb-4 text-sm w-3/5 bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"
-                  >
-                    {referedBy.map((item,index)=>{
-                      return <option key={index} value={item}>{item}</option>
-                    })}
-                  </select>
-                </div>
-
-                <div className='flex space-x-3'>
-                  <input type="date" name="date" id="date" className='px-3 py-2 mt-1 mb-4 text-sm w-1/2 bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
-                  <input type="time" name="time" id="time" className='px-3 py-2 mt-1 mb-4 text-sm w-1/2 bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
-                </div>
-
-              </div>
-
-
-
-              <div className='flex space-x-3 justify-between items-center'>
-
-                <div className='flex items-center gap-2 w-96'>
-                  <label className="font-semibold w-20 text-sm text-gray-600 pb-1">Techantion</label>
-                  <input placeholder='John Doe' name="techantion " id='techantion' type="number" className="px-3 py-2 ml-6 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
-                </div>
-
-
-
-                <div className='flex items-center'>
-                  <label className="font-semibold w-40 text-sm text-gray-600 pb-1">Product Type</label>
+                <div className='flex flex-col w-1/3'>
+                  <label className="font-semibold w-52 text-sm text-gray-600 pb-1">Product Type</label>
                   <select
                     id="productType"
                     name="productType"
@@ -110,10 +80,38 @@ const Invoices = () => {
                     })}
                   </select>
                 </div>
+
+                <div className='flex flex-col w-1/4'>
+                  <label className="font-semibold w-20 text-sm text-gray-600 pb-1">Techantion</label>
+                  <input placeholder='John Doe' name="techantion " id='techantion' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                </div>
+
+
+
                 
 
+              </div>
 
-                <div className='flex items-center'>
+
+              <div className='flex space-x-5 justify-between items-center'>
+
+                
+
+                <div className='flex flex-col w-1/3'>
+                  <label className="font-semibold w-20 text-sm text-gray-600 pb-1">Data Type</label>
+                  <select
+                    id="dataType"
+                    name="dataType"
+                    className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"
+                  >
+                    {dataType.map((item,index)=>{
+                      return <option key={index} value={item}>{item}</option>
+                    })}
+                  </select>
+                </div>
+
+
+                <div className='flex flex-col w-1/3'>
                   <label className="font-semibold w-52 text-sm text-gray-600 pb-1">Related Product</label>
                   <select
                     id="relatedProduct"
@@ -126,30 +124,51 @@ const Invoices = () => {
                   </select>
                 </div>
 
+                <div className='flex flex-col w-1/4'>
+                  <label className="font-semibold text-sm text-gray-600 pb-1">Warrenty</label>
+                  <input readOnly placeholder='warrenty' name="warrenty " id='warrenty' type="number" className="cursor-not-allowed px-3 py-2 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                </div>
+
               </div>
             </div>
 
-            <div className='flex items-center justify-between'>
-              <div className='flex items-center gap-3'>
-                <label className="font-semibold w-20 text-sm text-gray-600 pb-1">Data Type</label>
+            <div className='flex items-center space-x-5 justify-between'>
+
+              <div className='flex flex-col w-1/2'>
+                <label className="font-semibold  text-sm text-gray-600 pb-1">Title or short description</label>
+                <input placeholder='title or short description' name="shortDesc " id='techantion' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+              </div>
+
+
+              <div className='flex flex-col w-1/3'>
+                <label className="font-semibold w-52 text-sm text-gray-600 pb-1">Customer Phone No</label>
                 <select
-                  id="dataType"
-                  name="dataType"
+                  id="customerPhone"
+                  name="customerPhone"
                   className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"
                 >
-                  {dataType.map((item,index)=>{
+                  {referedBy.map((item,index)=>{
                     return <option key={index} value={item}>{item}</option>
                   })}
                 </select>
               </div>
-              <div className='flex items-center gap-2'>
-                  <label className="font-semibold  text-sm text-gray-600 pb-1">Title or short description</label>
-                  <input placeholder='title or short description' name="shortDesc " id='techantion' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+
+              <div className='flex flex-col w-1/2'>
+                  <label className="font-semibold text-sm text-left text-gray-600 pb-1">Date/Time</label>
+                  <div className='flex space-x-3'>
+                    <input type="date" name="date" id="date" className='px-3 py-2 mt-1 mb-4 text-sm w-1/2 bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
+                    <input type="time" name="time" id="time" className='px-3 py-2 mt-1 mb-4 text-sm w-1/2 bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
+                  </div>
                 </div>
-              <div>
-                <input readOnly placeholder='warrenty' name="warrenty " id='warrenty' type="number" className="cursor-not-allowed px-3 py-2 ml-6 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
-              </div>
+
+              
+
+
+              
+
             </div>
+
+            
             
 
             <div className=''>
@@ -157,14 +176,13 @@ const Invoices = () => {
             </div>
 
             <div className='flex space-x-3 justify-between items-center'>
-              <button type="submit" className='flex p-[8px] px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
-                {/* <AiOutlineCamera className='text-4xl text-[#ee8e00]'/> */}
-                <AiOutlineCamera className='text-3xl'/>
+              <button type="submit" className='flex p-[7px] rounded-md px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
+                <AiOutlineCamera className='text-xl'/>
                 Open Camera
               </button>
 
               <div className='flex items-center ml-auto'>
-                  <button type="submit" className='flex p-[8px] px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
+                  <button type="submit" className='flex p-[8px] rounded-md px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
                     <BsFillCheckCircleFill className='text-sm'/>
                       Create Ticket
                   </button>
