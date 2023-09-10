fe251414140883d83e24146a7575ae7bacfcdef5
faixanmalik <faixan.malik554@gmail.com>
2023-09-10 17:17:35 +0500

    add some camera icons

--- src/pages/menu/tickets.js
+++ src/pages/menu/tickets.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {BsFillCheckCircleFill} from 'react-icons/bs'
-import { AiOutlineCamera } from 'react-icons/ai'
+import {BsCameraFill, BsFillCheckCircleFill} from 'react-icons/bs'
 
 const Tickets = () => {
 
@@ -57,9 +56,9 @@ const Tickets = () => {
                     <div className='flex space-x-3 items-center ml-auto mr-5'>
                       <label className="font-semibold text-sm text-gray-600 pb-1">Set Printing</label>
                       <label className="font-semibold text-sm text-gray-600 pb-1">1</label>
-                      <input checked id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <input readOnly checked id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                       <label className="font-semibold text-sm text-gray-600 pb-1">2</label>
-                      <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      <input readOnly id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                     </div>
 
                   </div>
@@ -69,12 +68,12 @@ const Tickets = () => {
 
               <div className='flex space-x-5 justify-between'>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/6'>
                   <label className="font-semibold text-sm text-left text-gray-600 pb-1">Product Number</label>
                   <input placeholder='1234567890' name="productNumber" id='productNumber' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/6'>
                   <label className="font-semibold w-52 text-sm text-gray-600 pb-1">Customer Phone No</label>
                   <select
                     id="customerPhone"
@@ -87,47 +86,49 @@ const Tickets = () => {
                   </select>
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-[130px]'>
                   <label className="font-semibold text-sm text-left text-gray-600 pb-1">First Name</label>
                   <input placeholder='John' name="firstName" id='firstName' type="text" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-[130px]'>
                   <label className="font-semibold text-sm text-left text-gray-600 pb-1">Last Name</label>
                   <input placeholder='Doe' name="lastName" id='lastName' type="text" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/6'>
                   <label className="font-semibold text-sm text-left text-gray-600 pb-1">Comapny Name</label>
                   <input placeholder='John pvt.ltd' name="companyName" id='companyName' type="text" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
-              </div>
 
-
-              <div className='flex space-x-5 justify-between items-center'>
-
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/6'>
                   <label className="font-semibold text-sm text-left text-gray-600 pb-1">Email</label>
                   <input placeholder='abc@example.com' name="email" id='email' type="email" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+              </div>
+
+              
+
+
+              <div className='flex space-x-5 justify-between items-center'>
+
+              
+                <div className='flex flex-col w-1/3'>
                   <label className="font-semibold w-20 text-sm text-gray-600 pb-1">Techantion</label>
                   <input placeholder='John Doe' name="techantion " id='techantion' type="number" className="px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/3'>
                   <label className="font-semibold text-sm text-gray-600 pb-1">Warrenty</label>
                   <input readOnly placeholder='warrenty' name="warrenty " id='warrenty' type="number" className="cursor-not-allowed px-3 py-2 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
                 </div>
 
 
-                <div className='flex flex-col w-1/4'>
+                <div className='flex flex-col w-1/3'>
                   {/* <label className="font-semibold text-sm text-gray-600 pb-1">Warrenty</label>
-                  <input readOnly placeholder='warrenty' name="warrenty " id='warrenty' type="number" className="cursor-not-allowed px-3 py-2 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" /> */}
+                  <input placeholder='warrenty' name="warrenty " id='warrenty' type="number" className="cursor-not-allowed px-3 py-2 mt-1 mb-4 text-sm bg-slate-100 bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" /> */}
                 </div>
-
-                
                 
 
               </div>
@@ -189,16 +190,18 @@ const Tickets = () => {
             </div>
 
             <div className='flex space-x-3 justify-between items-center'>
-              <button type="submit" className='flex p-[7px] rounded-md px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
-                <AiOutlineCamera className='text-xl'/>
-                Open Camera
-              </button>
+              <div className='flex space-x-2'>
+                <BsCameraFill className='text-6xl text-black'/>
+                <BsCameraFill className='text-6xl text-black'/>
+                <BsCameraFill className='text-6xl text-black'/>
+              </div>
+             
 
               <div className='flex items-center ml-auto'>
-                  <button type="submit" className='flex p-[8px] rounded-md px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
-                    <BsFillCheckCircleFill className='text-sm'/>
-                      Create Ticket
-                  </button>
+                <button type="submit" className='flex p-[8px] rounded-md px-3 items-center gap-2 bg-[#ee8e00] text-white cursor-pointer'>
+                  <BsFillCheckCircleFill className='text-sm'/>
+                    Create Ticket
+                </button>
               </div>
             </div>
 
