4784fe41bf97fc2b23988eda8795117e51646d76
faixanmalik <faixan.malik554@gmail.com>
2023-09-11 15:53:24 +0500

    fixing

--- src/pages/menu/tickets.js
+++ src/pages/menu/tickets.js
@@ -60,16 +60,6 @@ const Tickets = () => {
                     </div>
                   </div>
 
-                  <div className='flex flex-col'>
-                    <div className='flex space-x-3 items-center ml-auto'>
-                      <label className="font-semibold text-sm text-gray-600 pb-1">Set Printing</label>
-                      <label className="font-semibold text-sm text-gray-600 pb-1">1</label>
-                      <input readOnly checked id="checked-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                      <label className="font-semibold text-sm text-gray-600 pb-1">2</label>
-                      <input readOnly id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    </div>
-
-                  </div>
                 </div>
               </div>
 
