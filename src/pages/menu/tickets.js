d22acb7bb7f5dab4899981c6f22cd4ed71634ddd
faixanmalik <faixan.malik554@gmail.com>
2023-09-15 12:08:31 +0500

    add table in ticket page

--- src/pages/menu/tickets.js
+++ src/pages/menu/tickets.js
@@ -8,6 +8,7 @@ import { LuSettings2 } from 'react-icons/lu';
 const Tickets = () => {
 
   const [search, setSearch] = useState('')
+  const [selectedIds, setSelectedIds] = useState([]);
 
   const handleChange = (e)=>{
     if(e.target.name === 'search'){
@@ -15,9 +16,29 @@ const Tickets = () => {
     }
   }
 
+  function handleRowCheckboxChange(e, id) {
+    if (e.target.checked) {
+      setSelectedIds([...selectedIds, id]);
+    } else {
+      setSelectedIds(selectedIds.filter(rowId => rowId !== id));
+    }
+  }
+
   let status = [
     'Resolved',
     'Not Resolved',
+  ]
+
+  let tableData = [
+    {
+      id: 4201,
+      customer: 'Walkin Customer',
+      subject: 'Ticket Title',
+      status: 'In-Progress',
+      lastUpdated:6,
+      created: '25-08-2023',
+      issue: 'Other'
+    },
   ]
 
   return (
@@ -145,6 +166,77 @@ const Tickets = () => {
             </div>
 
           </div>
+
+
+          <div className="overflow-x-auto shadow-sm mt-2">
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-200">
+                  <tr>
+                    <th scope="col" className="p-4">
+                      <div className="flex items-center">
+                        <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      </div>
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Sr.
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Customer
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Subject
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Status
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Last Updated
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Created
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Issue
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  
+                  {tableData.map((item, index)=>{
+                    return <tr key={index} className="bg-white border-b hover:bg-gray-50">
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input id="checkbox-table-search-1" type="checkbox" onChange={e => handleRowCheckboxChange(e, item._id)} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                      </div>
+                    </td>
+                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {item.id}
+                    </td>
+                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {item.customer}
+                    </td>
+                    <td className="px-6 py-4">
+                        {item.subject}
+                    </td>
+                    <td className="px-6 py-4">
+                        {item.status}
+                    </td>
+                    <td className="px-6 py-4">
+                        {item.lastUpdated}
+                    </td>
+                    <td className="px-6 py-4">
+                        {item.created}
+                    </td>
+                    <td className="px-6 py-4">
+                        {item.issue}
+                    </td>
+                  </tr>})}
+
+                </tbody>
+
+              </table>
+            </div>
         </div>
       </div>
         
