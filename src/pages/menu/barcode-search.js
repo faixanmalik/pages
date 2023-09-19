cad724e47ea6da3535f84202a99d30fad184ebdf
faixanmalik <faixan.malik554@gmail.com>
2023-09-19 15:53:13 +0500

    add gray icons with field

--- src/pages/menu/barcode-search.js
+++ src/pages/menu/barcode-search.js
@@ -1,6 +1,6 @@
 import {React, useState} from 'react'
 import { AiOutlineUser } from 'react-icons/ai';
-import { BsFillArrowRightCircleFill, BsTelephone } from 'react-icons/bs';
+import { BsFillArrowRightCircleFill, BsSearch, BsTelephone } from 'react-icons/bs';
 import { MdOutlineNumbers } from 'react-icons/md';
 
 const BarcodeSearch = () => {
@@ -46,37 +46,45 @@ const BarcodeSearch = () => {
             <div className='flex my-6 justify-between'>
 
                 <div className='flex space-y-5 flex-col'>
-                    <div className="relative w-full">
+                    
+                    {/* <div className="relative w-full">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
                         <input value={search} onChange={handleChange} type="text" id="search" name='search' className="block w-full p-2 pl-10 border border-gray-600 text-sm text-gray-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." required/>
+                    </div> */}
+
+                    <div class="flex">
+                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
+                            <BsSearch className='text-lg mt-1'/>
+                        </span>
+                        <input placeholder="Search..." value={search} onChange={handleChange} type="text" id="search" name='search' className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-600 p-2.5 "  required/>
                     </div>
 
-                    <div className="relative w-full">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <MdOutlineNumbers className='text-xl mt-1 bg-gray-50'/>
-                        </div>
-                        <input placeholder='Product Name' name="productName" id='productName' type="text" className="px-3 py-2 w-9/12 mt-1 pl-10 text-sm bg-gray-50 resize-none text-gray-700 outline-none border border-gray-600 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                    <div class="flex">
+                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
+                            <MdOutlineNumbers className='text-lg mt-1'/>
+                        </span>
+                        <input placeholder='Product Name' type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-600 p-2.5"/>
                     </div>
 
                     
                     <div className='flex space-x-3'>
 
-                        <div className="relative w-full">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <AiOutlineUser className='text-xl mt-1 bg-gray-50'/>
-                            </div>
-                            <input placeholder='Supplier Name' name="supplierName" id='supplierName' type="text" className="px-3 py-2 mt-1 pl-10 text-sm bg-gray-50 resize-none text-gray-700 outline-none border border-gray-600 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                        <div class="flex w-full">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
+                                <AiOutlineUser className='text-lg mt-1'/>
+                            </span>
+                            <input placeholder='Supplier Name' type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-600 p-2.5"/>
                         </div>
 
-                        <div className="relative w-full">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <BsTelephone className='text-lg mt-1 bg-gray-50'/>
-                            </div>
-                            <input placeholder='Supplier No' name="supplierNo" id='supplierNo' type="number" className="px-3 py-2 mt-1 pl-10  text-sm bg-gray-50 resize-none text-gray-700 outline-none border border-gray-600 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                        <div class="flex w-full">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
+                                <BsTelephone className='text-lg mt-1'/>
+                            </span>
+                            <input placeholder='Supplier No' type="text" id="website-admin" class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-600 p-2.5"/>
                         </div>
 
 
