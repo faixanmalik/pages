103d31f35cd8a9acb63701f0d0a1c29a113fe03d
faixanmalik <faixan.malik554@gmail.com>
2023-09-19 16:12:48 +0500

    height fix image

--- src/pages/menu/barcode-search.js
+++ src/pages/menu/barcode-search.js
@@ -1,6 +1,6 @@
 import {React, useState} from 'react'
 import { AiOutlineUser } from 'react-icons/ai';
-import { BsFillArrowRightCircleFill, BsSearch, BsTelephone } from 'react-icons/bs';
+import { BsCalendarDate, BsFillArrowRightCircleFill, BsSearch, BsTelephone } from 'react-icons/bs';
 import { MdOutlineNumbers } from 'react-icons/md';
 
 const BarcodeSearch = () => {
@@ -46,15 +46,6 @@ const BarcodeSearch = () => {
             <div className='flex my-6 justify-between'>
 
                 <div className='flex space-y-5 flex-col'>
-                    
-                    {/* <div className="relative w-full">
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                            </svg>
-                        </div>
-                        <input value={search} onChange={handleChange} type="text" id="search" name='search' className="block w-full p-2 pl-10 border border-gray-600 text-sm text-gray-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." required/>
-                    </div> */}
 
                     <div class="flex">
                         <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
@@ -91,7 +82,12 @@ const BarcodeSearch = () => {
                     </div>
 
                     <div className='flex space-x-3'>
-                        <input type="date" name="date" id="date" className='px-3 py-2 text-sm bg-gray-50 resize-none text-gray-700 outline-none border border-gray-600 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
+                        <div class="flex w-full">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-600 rounded-l-md">
+                                <BsCalendarDate className='text-lg mt-1'/>
+                            </span>
+                            <input type="date" name="date" id="date" className='rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-600 p-2.5' />
+                        </div>
 
                         <input placeholder='36 Months' name="supplierNo" id='supplierNo' type="number" className="px-3 py-2 text-sm bg-gray-50 resize-none text-gray-700 outline-none border border-gray-600 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
 
@@ -119,9 +115,9 @@ const BarcodeSearch = () => {
 
                 <div className='flex flex-col space-y-3 w-1/3'>
 
-                    <div className='border border-gray-600'>
+                    <div className='border border-gray-600 rounded-lg'>
 
-                        <div className='text-center py-20'>
+                        <div className='text-center py-10 '>
 
                             {warrenty.warrentyStatus === true 
                                 ? <div>
@@ -139,9 +135,7 @@ const BarcodeSearch = () => {
                     </div>
 
 
-                    <div className='ml-auto'>
-                        <button type="submit" className='flex py-2 font-semibold rounded-md px-6 items-center gap-2 bg-white text-[#ee8e00] hover:bg-[#ee8e00] hover:text-white border border-[#ee8e00] cursor-pointer'>Close</button>
-                    </div>
+                    
 
                     
                 </div>
@@ -152,6 +146,11 @@ const BarcodeSearch = () => {
           </div>
 
           
+        </div>
+
+        <div className='mt-4'>
+            {/* <button type="submit" className='flex py-2 font-semibold rounded-md px-6 items-center gap-2 bg-white text-[#ee8e00] hover:bg-[#ee8e00] hover:text-white border-2 border-[#ee8e00] cursor-pointer'>Close</button> */}
+            <button type="submit" className=' py-1 mt-3 rounded-xl px-7 text-black border-2 border-[#ee8e00] hover:bg-[#ee8e00] hover:text-white cursor-pointer'>Close</button>
         </div>
 
       </div>
