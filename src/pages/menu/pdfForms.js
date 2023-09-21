f7e8a2b7277c3d53cd24ee29a51e9f7604c06e83
faixanmalik <faixan.malik554@gmail.com>
2023-09-21 17:36:45 +0500

    done with pdf forms

--- src/pages/menu/pdfForms.js
+++ src/pages/menu/pdfForms.js
@@ -14,39 +14,30 @@ const PdfForms = () => {
         {/* <h1 className='text-gray-600 font-semibold'>Basic Info</h1> */}
         <div className='w-full space-y-3 items-center mt-10'>
 
+          <img className='w-full' src='/PdfFormHeader.jpg' alt="Header" srcset="" />
+
           <div className='w-full space-y-3 items-center'>
 
             <div className='flex my-6 justify-between'>
 
-                <div className='flex flex-col w-[150px]'>
-                    <textarea placeholder='Ticket Status: Open, Progress, Closed' id="message" rows="3" cols={60} class="placeholder:font-semibold px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"></textarea>
+                <div className='w-1/4'>
+                  <h1 className="uppercase w-1/2 text-center py-2 text-lg font-bold bg-white bg-opacity-50 resize-none text-gray-500 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out">
+                    Open
+                  </h1>
                 </div>
 
-                <div className='flex flex-col'>
-                    <div className='flex'>
-                        <textarea placeholder='Company Info - name, address, address etc' id="message" rows="6" cols={60} class="placeholder:font-semibold px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"></textarea>
-                    </div>
-                    <div className='flex justify-center'>
-                        <textarea placeholder='Ticket number: xxxxxx' id="ticketNo" rows="3" cols={25} class="placeholder:font-bold placeholder:text-xl placeholder:items-center placeholder:text-gray-700 px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"></textarea>
-                    </div>
+                <div className='flex flex-col justify-center'>
+                  <textarea placeholder='Ticket number: xxxxxx' id="ticketNo" rows="3" cols={25} class="placeholder:font-bold placeholder:text-xl placeholder:items-center placeholder:text-gray-700 px-3 py-2 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out"></textarea>
                 </div>
 
 
-                <div className='flex flex-col w-[250px]'>
+                <div className='flex flex-col space-y-2 w-[250px]'>
 
-                    <div role="status" class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
-                        <div class="flex items-center justify-center w-full h-40 bg-gray-300 rounded sm:w-full">
-                            <svg class="w-10 h-10 text-gray-200 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-                            </svg>
-                        </div>
-                    </div>
-
-                    <input placeholder='technician: xxxxxxx' name="productNumber" id='productNumber' type="number" className="px-3 py-2 mt-1 mb-1 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
+                    <input placeholder='technician: xxxxxxx' name="productNumber" id='productNumber' type="number" className="px-3 py-2 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out" />
 
                     <div className='flex space-x-2'>
-                        <input type="date" name="date" id="date" className='px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
-                        <input type="time" name="time" id="time" className='px-3 py-2 mt-1 mb-4 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
+                        <input type="date" name="date" id="date" className='px-3 py-2 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
+                        <input type="time" name="time" id="time" className='px-3 py-2 text-sm bg-white bg-opacity-50 resize-none text-gray-700 outline-none border border-gray-300 focus:border-blue-500 focus:shadow-lg shadow-blue-500 rounded-lg  transition-colors duration-200 ease-in-out' />
                     </div>
                 </div>
 
