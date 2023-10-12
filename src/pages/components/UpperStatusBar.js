28f4328fb43a5903fdb539079aa7e25b669cb736
faixanmalik <faixan.malik554@gmail.com>
2023-10-12 17:58:38 +0500

    chnage the header search bar button

--- src/pages/components/UpperStatusBar.js
+++ src/pages/components/UpperStatusBar.js
@@ -15,13 +15,13 @@ const UpperStatusBar = () => {
 
       <div className='pl-10 flex flex-1 gap-6 items-center'>
         <h1 className='text-white font-semibold'>{headerText}</h1>
-        <div className="relative items-center">
+        <div className="relative items-center ">
           <div className="absolute top-3 left-0 flex items-center pl-3 pointer-events-none">
               <svg className="w-4 h-4 text-gray-500 dark:text-[#d4d4d4]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
           </div>
-          <input type="search" id="default-search" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#222222] dark:border-[#8e8e8e] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="search all the things..." required/>
+          <input type="search" id="default-search" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-500 rounded-lg bg-[#222222] focus:ring-blue-500 focus:border-blue-500 dark:bg-[#222222] dark:border-[#8e8e8e] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="search all the things..." required/>
         </div>
         <svg width="38" height="32" viewBox="0 0 38 39" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="38" height="39" rx="6" fill="#464646"/>
