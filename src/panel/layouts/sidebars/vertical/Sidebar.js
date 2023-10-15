c6e62ec09c4e82ef2112d0cfec712d14382f0b4a
faixanmalik <faixan.malik554@gmail.com>
2023-10-15 16:21:01 +0500

    add items in sidebar

--- src/panel/layouts/sidebars/vertical/Sidebar.js
+++ src/panel/layouts/sidebars/vertical/Sidebar.js
@@ -2,11 +2,14 @@ import { React, useState } from 'react'
 import { useRouter } from "next/router";
 import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar';
 
-import { MdEmail, MdSms} from 'react-icons/md'
+import { MdEmail, MdInventory2, MdSms} from 'react-icons/md'
 import {IoSettings} from 'react-icons/io5'
 
-import { BsCurrencyDollar} from 'react-icons/bs'
+import { BsBuildingFillAdd, BsCurrencyDollar} from 'react-icons/bs'
 import { LuDatabaseBackup } from 'react-icons/lu';
+import { FiUsers } from 'react-icons/fi';
+import { FaUserFriends } from 'react-icons/fa';
+import { BiSolidPackage } from 'react-icons/bi';
 
 
 
@@ -25,20 +28,32 @@ const Sidebar2 = ({ showMobilemenu }) => {
       <Sidebar width='255px' className='min-h-screen mt-10'>
 
         <Menu className='bg-white'>
-          <MenuItem href="/menu/adminSettings/settings" icon={<IoSettings className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
+          <MenuItem href="/menu/adminSettings/settings" icon={<IoSettings className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'  } >
             System settings
           </MenuItem>
-          <MenuItem href="/menu/adminSettings/smsTemplates" icon={<MdSms className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 bg-zinc-50 font-medium' : 'text-gray-600 font-medium'} >
+          <MenuItem href="/menu/adminSettings/smsTemplates" icon={<MdSms className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
             SMS templates
           </MenuItem>
-          <MenuItem href="/menu/adminSettings/emailsTemplates" icon={<MdEmail className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 bg-zinc-50 font-medium' : 'text-gray-600 font-medium'} >
+          <MenuItem href="/menu/adminSettings/emailsTemplates" icon={<MdEmail className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
             Emails templates
           </MenuItem>
-          <MenuItem href="/menu/adminSettings/currency" icon={<BsCurrencyDollar className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 bg-zinc-50 font-medium' : 'text-gray-600 font-medium'} >
+          <MenuItem href="/menu/adminSettings/currency" icon={<BsCurrencyDollar className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
             Currency
           </MenuItem>
-          <MenuItem href="/menu/adminSettings/backup" icon={<LuDatabaseBackup className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 bg-zinc-50 font-medium' : 'text-gray-600 font-medium'} >
+          <MenuItem href="/menu/adminSettings/backup" icon={<LuDatabaseBackup className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
             Backup
+          </MenuItem>
+          <MenuItem href="/menu/adminSettings/users" icon={<FaUserFriends className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
+            Users
+          </MenuItem>
+          <MenuItem href="/menu/adminSettings/companies" icon={<BsBuildingFillAdd className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
+            Companies
+          </MenuItem>
+          <MenuItem href="/menu/adminSettings/inventory" icon={<MdInventory2 className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
+            Inventory
+          </MenuItem>
+          <MenuItem href="/menu/adminSettings/packages" icon={<BiSolidPackage className='text-lg'/>} className={ location === '/panel' ?  'text-indigo-700 font-medium' : 'text-gray-600 font-medium'} >
+            Packages
           </MenuItem>
         </Menu>
 
