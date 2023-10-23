903bd586ab38dc9be4d1efd08a90bcee7a9a0f25
faixanmalik <faixan.malik554@gmail.com>
2023-10-23 11:16:30 +0500

    add key in mapping

--- src/pages/menu/leads.js
+++ src/pages/menu/leads.js
@@ -26,7 +26,7 @@ const Leads = () => {
 
         <div className='flex flex-wrap px-10 space-x-3'>
           {cards.map((item, index)=>{
-            return <Card className="mt-6 w-56 h-40">
+            return <Card key={index} className="mt-6 w-56 h-40">
             <CardBody className='px-5 py-2'>
 
               <div className='flex justify-between items-center'>
